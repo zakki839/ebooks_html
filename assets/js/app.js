3a6baf33043025893a8af82bac9b238699ef9442
zakki839 <zakkinazzi@gmail.com>
2024-10-17 17:55:23 +0530

    inserted code

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+const books=[
+    {
+title:"the great gatsby",
+ author:"f.scott frindge",
+  price:"300",
+image:"https://via.placeholder.com/200x120.png?text=Book+1"
+},
+{
+    title:"1984",
+    author:"f.scott frindge",
+    price:"250",
+    image:"https://via.placeholder.com/200x120.png?text=Book+2"
+},
+{
+    title:"to kill a mockingbird",
+    author:"f.scott frindge",
+    price:"350",
+    image:"https://via.placeholder.com/200x120.png?text=Book+3"
+},
+{
+    title:"the pride and prejudice",
+    author:"f.scott frindge",
+    price:"390",
+    image:"https://via.placeholder.com/200x120.png?text=Book+4"
+},
+
+{
+    title:"the great gatsby",
+    author:"f.scott frindge",
+    price:"300",
+    image:"https://via.placeholder.com/200x120.png?text=Book+5"
+}
+];
+const booksContainer=document.getElementById("books");
+console.log(booksContainer);
